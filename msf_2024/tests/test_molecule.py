e34bc4652a7a8c47f089f65c2e021749e00b0a71
cabb99 <carlos.bueno@rice.edu>
2024-07-24 11:40:11 -0400

    Sets up a fixture

--- msf_2024/tests/test_molecule.py
+++ msf_2024/tests/test_molecule.py
@@ -2,24 +2,24 @@ import msf_2024
 import numpy as np
 import pytest
 
-def test_build_bond_list():
-    coordinates=np.array([[-1.40,0,0],
-                         [0,0,1.4],
-                         [0,0,0],
-                         [0,1.4,0],
-                         [0,-1.4,0]])
+@pytest.fixture
+def methane_molecule():
+    coordinates=np.array([[-1.4000001,0,0],
+                            [0,0,1.4],
+                            [0,0,0],
+                            [0,1.4,0],
+                            [0,-1.4,0]])
+    return coordinates
+
+def test_build_bond_list(methane_molecule):
+    coordinates=methane_molecule
     bonds=msf_2024.build_bond_list(coordinates,1.5,0)
     assert len(bonds)==4, 'Expected 4 bonds'
     for bond_length in bonds.values():
         assert pytest.approx(bond_length) == 1.4, 'Expected a bond length of 1.4'
 
-def test_fail_build_bond_list():
-    coordinates=np.array([[-1.40,0,0],
-                        [0,0,1.4],
-                        [0,0,0],
-                        [0,1.4,0],
-                        [0,-1.4,0]])
-    
+def test_fail_build_bond_list(methane_molecule):
+    coordinates=methane_molecule
     msf_2024.build_bond_list(coordinates=coordinates,max_bond=0)
     with pytest.raises(ValueError):
         msf_2024.build_bond_list(coordinates=coordinates,max_bond=-1)
